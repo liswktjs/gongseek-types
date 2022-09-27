0b02567498a39204d7a2d7e0eb8a679dc7a3c87d
JASUN LEE <liswktjs@gmail.com>
2022-09-27 16:17:02 +0900

    feat: DateFormat타입과 React children 타입 선언

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,10 @@
-declare type gongseek = "A+";
+import * as React from "react";
+import { Month, Day, DateSuffix } from "./util";
+export declare type VoteDateFormat = `${string}-${Month}-${Day}`;
+export declare type AfterWriteDate = `${string}${DateSuffix}`;
+export declare type PropsWithStrictChildren<P, T extends React.ReactNode = React.ReactNode> = P & {
+    children: T;
+};
+export declare type PropsWithOptionalChildren<P, T extends React.ReactNode = React.ReactNode> = P & {
+    children?: T;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-export type gongseekType = "A+";
+import * as React from "react";
+import {
+  Month,
+  Day,
+  DateSuffix,
+} from "./util";
+
+export type VoteDateFormat =
+  `${string}-${Month}-${Day}`;
+
+export type AfterWriteDate =
+  `${string}${DateSuffix}`;
+
+export type PropsWithStrictChildren<
+  P,
+  T extends React.ReactNode = React.ReactNode
+> = P & { children: T };
+
+export type PropsWithOptionalChildren<
+  P,
+  T extends React.ReactNode = React.ReactNode
+> = P & { children?: T };
